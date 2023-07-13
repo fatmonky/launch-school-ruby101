1a5183c5133f0baefd131f9f8abb3dcfe65d6216
PJ Teh <peijing.teh.dev@gmail.com>
2023-07-13 16:18:58 +0800

    RPS Bonus Features #1 & #2. #3 (keeping score) still WIP. Rubocop passed.

--- lesson_2/rock_paper_scissors_lizard_spock.rb
+++ lesson_2/rock_paper_scissors_lizard_spock.rb
@@ -1,5 +1,7 @@
 VALID_CHOICES = ["rock", "paper", "scissors", "lizard", "spock"]
 
+VALID_SHORTCUTS = ["r", "p", "sc", "l", "sp"]
+
 WINNING_MOVES = {
   "rock" => {
     "scissors" => true,
@@ -23,9 +25,15 @@ WINNING_MOVES = {
   }
 }
 
-def test_method
-  prompt("test message")
-end
+CHOICE_MESSAGE = <<-MSG
+Choose one option:
+-rock (or "r")
+-paper (or "p")
+-scissors (or "sc")
+-lizard (or "l"')
+-spock (or "sp"')
+
+MSG
 
 def prompt(message)
   Kernel.puts("=> #{message}")
@@ -45,18 +53,26 @@ def display_results(player, computer)
   end
 end
 
-loop do
+def player_choice
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt(CHOICE_MESSAGE)
     choice = Kernel.gets().chomp().downcase
-
-    if VALID_CHOICES.include?(choice)
-      break
-    else
+    if (VALID_CHOICES.include?(choice) == false) &&
+       (VALID_SHORTCUTS.include?(choice) == false)
       prompt("That's not a valid choice.")
+    elsif VALID_SHORTCUTS.include?(choice)
+      choice = VALID_CHOICES[VALID_SHORTCUTS.find_index(choice)]
+      break
+    elsif VALID_CHOICES.include?(choice)
+      break
     end
   end
+  choice
+end
+
+loop do
+  choice = player_choice
 
   computer_choice = VALID_CHOICES.sample
 
